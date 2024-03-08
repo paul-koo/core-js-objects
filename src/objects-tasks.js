63d72222d84fe4f24865c85a083b661a1d21c562
paul-koo <pavelkoltun999@gmail.com>
2024-03-08 11:49:56 +0300

    feat: 18. passing, 2 pending, 1 failing

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -130,8 +130,15 @@ function makeImmutable(obj) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const arrWord = [];
+  const keyObject = Object.keys(lettersObject);
+  keyObject.forEach((key) => {
+    lettersObject[key].forEach((element) => {
+      arrWord[element] = key;
+    });
+  });
+  return arrWord.toString().replaceAll(',', '');
 }
 
 /**
@@ -148,8 +155,19 @@ function makeWord(/* lettersObject */) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  let money = 0;
+  let flag = true;
+  queue.forEach((element) => {
+    if (money < element - 25) {
+      flag = false;
+    }
+    money += 25;
+  });
+  if (flag) {
+    return true;
+  }
+  return false;
 }
 
 /**
